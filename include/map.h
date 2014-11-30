d671fae3fdd8a584d7bf572d9a5e31943d2018e9
Gurdil <jb.cr.philippe@gmail.com>
2014-12-01 00:44:46 +0100

    Best error message

--- include/map.h
+++ include/map.h
@@ -14,6 +14,7 @@
 #include <stdexcept>
 #include <iostream>
 #include <complex>
+#include <string>
 
 class DLmap
 {
--- src/map.cpp
+++ src/map.cpp
@@ -50,7 +50,7 @@ int DLmap::convert(int i, int j)
 {
 	if(i<0 || i>(size-1) || j<0 || j>(size-1))
 	{
-		throw std::out_of_range("Out of range in map");
+		throw std::out_of_range("Out of range in map : "+std::to_string (i)+", "+std::to_string (j));
 	}
 	return i*size + j;
 }
